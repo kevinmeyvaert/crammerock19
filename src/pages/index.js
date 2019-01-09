39372a574c4f8e71e8d8fb3e4c5b5babde71885b
kevinmeyvaert <hallo@ditiskev.in>
2019-01-09 18:31:56 +0100

    :art: Remove random artist widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react';
-import Link from 'gatsby-link';
 import get from 'lodash/get';
 import { graphql } from 'gatsby';
 
@@ -9,19 +8,14 @@ import {
   Template,
   Header,
   NewsItem,
-  Image,
 } from '../components';
 
 import { ellipsis } from '../util';
 import styles from './styles/index.module.css';
-import type { TArtist } from '../types';
-import { useGetRandomArtistInterval } from '../hooks';
 
 const RootIndex = (props) => {
-  const artists = get(props, 'data.allContentfulArtists.edges');
   const news = get(props, 'data.allContentfulNews.edges');
   const headerData = news[0].node;
-  const randomArtist: TArtist = useGetRandomArtistInterval(artists, 5000);
   return (
     <Template>
       <div>
@@ -41,11 +35,6 @@ const RootIndex = (props) => {
                 key={node.slug}
               />
             ))}
-            <div className={styles.randomArtist}>
-              <Image width={800} height={533} src={randomArtist.headerImage.file.url} alt={randomArtist.name} />
-              <h2><Link to={`/lineup/${randomArtist.slug}`}><span>{randomArtist.name}</span></Link></h2>
-              <h3><Link to="/lineup"><span>Ontdek de lineup!</span></Link></h3>
-            </div>
           </div>
         </div>
       </div>
