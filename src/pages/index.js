84425e20138c97f53663bd8bed670084827deedb
kevinmeyvaert <hallo@ditiskev.in>
2019-01-12 12:09:57 +0100

    :construction: More flex tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,10 @@ const RootIndex = (props) => {
           {news.slice(1, 2).map(({ node }) => (
             <Link to={`/news/${node.slug}`} key={node.slug}>
               <div className={styles.indexItem}>
+                <div className={styles.innerBorder}>
+                  <h2><span>{node.title}</span></h2>
+                </div>
                 <Image width={800} height={333} src={node.featuredImage.file.url} alt={node.title} />
-                <h2><span>{node.title}</span></h2>
               </div>
             </Link>
           ))}
