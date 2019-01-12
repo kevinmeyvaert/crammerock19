d5d87e1a757cbf780b65a3a47e4336b1c8d695d4
kevinmeyvaert <hallo@ditiskev.in>
2019-01-12 13:10:54 +0100

    :art: Use new contentblock component on info pages

--- src/pages/info.js
+++ src/pages/info.js
@@ -2,11 +2,10 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
 import get from 'lodash/get';
 import { graphql } from 'gatsby';
 
-import { Template, Header, Image } from '../components';
+import { Template, Header, ContentBlock } from '../components';
 
 import { randomArrayValue } from '../util';
 import styles from './styles/info.module.css';
@@ -26,12 +25,12 @@ const Info = (props) => {
       <div className={styles.wrapper}>
         <div className={styles.row}>
           {infoPages.map(infoItem => (
-            <Link to={`/info/${infoItem.node.slug}`} key={infoItem.node.title}>
-              <div className={styles.infoItem}>
-                <Image width={800} height={333} src={infoItem.node.headerImage.file.url} alt={infoItem.node.title} />
-                <h2><span>{infoItem.node.title}</span></h2>
-              </div>
-            </Link>
+            <ContentBlock
+              key={infoItem.node.slug}
+              title={infoItem.node.title}
+              link={`/info/${infoItem.node.slug}`}
+              contentfulImage={infoItem.node.headerImage.file.url}
+            />
           ))}
         </div>
       </div>
