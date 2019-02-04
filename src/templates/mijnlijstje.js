a8725727cebb302b22d52421906b052320dcce87
kevinmeyvaert <hallo@ditiskev.in>
2019-02-04 21:31:58 +0100

    :art: Add extra og tags

--- src/templates/mijnlijstje.js
+++ src/templates/mijnlijstje.js
@@ -46,19 +46,42 @@ const Lijstje = ({ listId }: { listId: string }) => {
         link="/lijstjestijd"
         image="https://images.ctfassets.net/nwp1ppgri1eh/5wdAe2GkfYSOOIMM4KIo2i/cf4a8d070a0d1ad03711cf35b8bf8232/large_4GXYu.jpg"
       />
-      <img width={1200} height={650} src={`https://firebasestorage.googleapis.com/v0/b/crammerock-1c990.appspot.com/o/lijstjes19%2F${listId}.png?alt=media&token=1cd92339-631d-416e-a741-d30e44d75a20`} />
-      <button type="button" onClick={() => handleShare(listId)} className={styles.button}>Deel je affiche!</button>
+      <img alt="Jouw lijstje!" width={1200} height={650} src={`https://firebasestorage.googleapis.com/v0/b/crammerock-1c990.appspot.com/o/lijstjes19%2F${listId}.png?alt=media&token=1cd92339-631d-416e-a741-d30e44d75a20`} />
+      <a target="_blank" rel="noopener noreferrer" href={`https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdeploy-preview-2--crammerock.netlify.com%2Fmijnlijstje%2F${listId}&amp;src=sdkpreparse`}>
+        <button type="button" className={styles.button}>Deel je affiche!</button>
+      </a>
+      <iframe
+        className={styles.iframe}
+        title="sharer"
+        src={`https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdeploy-preview-2--crammerock.netlify.com%2Fmijnlijstje%2F${listId}&amp;src=sdkpreparse`}
+      />
       {finished && (
         <Helmet
           title={`Het lijstje van ${name} | ${config.siteName}`}
           meta={[
             {
+              property: 'og:image',
+              content: `https://firebasestorage.googleapis.com/v0/b/crammerock-1c990.appspot.com/o/lijstjes19%2F${listId}.png?alt=media&token=1cd92339-631d-416e-a741-d30e44d75a20`,
+            },
+            {
+              property: 'og:image:secure_url',
+              content: `https://firebasestorage.googleapis.com/v0/b/crammerock-1c990.appspot.com/o/lijstjes19%2F${listId}.png?alt=media&token=1cd92339-631d-416e-a741-d30e44d75a20`,
+            },
+            {
               property: 'og:type',
               content: 'website',
             },
             {
-              property: 'og:image',
-              content: `https://firebasestorage.googleapis.com/v0/b/crammerock-1c990.appspot.com/o/lijstjes19%2F${listId}.png?alt=media&token=1cd92339-631d-416e-a741-d30e44d75a20`,
+              property: 'og:image:width',
+              content: 600,
+            },
+            {
+              property: 'og:image:height',
+              content: 314,
+            },
+            {
+              property: 'og:image:type',
+              content: 'image/png',
             },
             {
               property: 'og:title',
