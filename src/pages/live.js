85f7d44159ddd019359b283abf43cebc75261f25
kevinmeyvaert <hallo@behangmotief.be>
2019-08-07 07:28:34 +0200

    :sparkles: Add automated timer + fix first act of the day

--- src/pages/live.js
+++ src/pages/live.js
@@ -15,18 +15,23 @@ import {
 import styles from './styles/live.module.css';
 import { config } from '../config';
 
-const useLiveOnStage = (time, artists) => {
-  const now = moment('2019-09-06T13:35+02:00').add(time, 'hours');
-  const nowDay = now.format('dddd');
+const useLiveOnStage = (artists) => {
+  const [time, setTime] = useState(moment());
+  const nowDay = time.format('dddd');
   const [liveOnStage, setLiveOnStage] = useState();
   const mainArtists = artists.filter(({ node: artist }) => artist.stage.startsWith('Main'));
   const clubArtists = artists.filter(({ node: artist }) => artist.stage === 'Club');
-  console.log(clubArtists, mainArtists);
+  
   const getNodeFn = playingArtist => playingArtist && playingArtist.node;
   const findStageFn = (artist, stage) => (artist && artist.stage === stage) || (artist && artist.stage.startsWith(stage));
 
   useEffect(() => {
-    const nowPlaying = artists.filter(({ node: artist }) => now.isBetween(artist.showStart, artist.showEnd, null, '[]'));
+    const timer = setInterval(() => setTime(moment()), 10000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    const nowPlaying = artists.filter(({ node: artist }) => time.isBetween(artist.showStart, artist.showEnd, null, '[]'));
     const nextPlaying = nowPlaying.map(({ node: artistNow }) => {
       const stageList = artistNow.stage === 'Club' ? clubArtists : mainArtists;
       const nowPlayingIndex = stageList.findIndex(({ node: item }) => item.name === artistNow.name);
@@ -34,7 +39,7 @@ const useLiveOnStage = (time, artists) => {
     });
 
     setLiveOnStage({
-      time: now,
+      time,
       now: {
         club: nowPlaying.map(getNodeFn).find(a => findStageFn(a, 'Club')) || null,
         main: nowPlaying.map(getNodeFn).find(a => findStageFn(a, 'Main')) || null,
@@ -50,11 +55,10 @@ const useLiveOnStage = (time, artists) => {
 };
 
 const Live = (props) => {
-  const [time, setTime] = useState(1);
   const sortByTimeFn = (artistA, artistB) => new Date(artistA.node.showStart) - new Date(artistB.node.showStart);
   const artistData = get(props, 'data.allContentfulArtists2019.edges');
   const artists = artistData.sort(sortByTimeFn);
-  const liveOnStage = useLiveOnStage(time, artists);
+  const liveOnStage = useLiveOnStage(artists);
 
   return (
     <Template>
@@ -64,8 +68,6 @@ const Live = (props) => {
       />
       {liveOnStage && (
       <div className={styles.wrapper}>
-        <input type="range" step="1" min="1" max="36" value={time} onChange={(e) => setTime(e.currentTarget.value) } />
-        {moment(liveOnStage.time).format('dddd, HH:mm')}
         <div className={styles.row}>
           <div className={styles.item}>
             <h2>On Stage <span>Main</span></h2>
@@ -95,6 +97,7 @@ const Live = (props) => {
               />
             ) : (<p>Momenteel speelt er niks.</p>)}
           </div>
+          {moment(liveOnStage.time).isBefore('2019-09-08T02:05+02:00') && (
           <div className={styles.item}>
             <h2>Up Next <span>Main</span></h2>
             <LineUpItem
@@ -107,6 +110,8 @@ const Live = (props) => {
               stageFilter
             />
           </div>
+          )}
+          {moment(liveOnStage.time).isBefore('2019-09-08T01:00+02:00') && (
           <div className={styles.item}>
             <h2>Up Next <span>Club</span></h2>
             <LineUpItem
@@ -119,6 +124,7 @@ const Live = (props) => {
               stageFilter
             />
           </div>
+          )}
         </div>
       </div>
       )}
